03ee44467350a3ca624dd91fdb79646e40753ef9
bharath132 <47454425+bharath132@users.noreply.github.com>
2022-03-21 16:46:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var actions = [ 'forward', 'back', 'left', 'right']
 var lastaction;
 var pi = 3.14159;
 var moveinterval = 2; // 2 second movement interval
+var maxrandom = 5; // 0-5 seconds added to movement interval (randomly
 var host = data["ip"];
 var username = data["name"]
 var nightskip = data["auto-night-skip"]
